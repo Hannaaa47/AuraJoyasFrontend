02ee0f3603b116add524acd8c717005d236e8dff
Hanna Amador <152832888+Hannaaa47@users.noreply.github.com>
2026-05-28 00:15:36 +0000

    render

--- proyectoUnidad3y4/src/modules/auth/services/correoService.js
+++ proyectoUnidad3y4/src/modules/auth/services/correoService.js
@@ -1,5 +1,5 @@
 export async function enviarCorreoContacto({ nombre, correo, mensaje }) {
-  const res = await fetch('http://127.0.0.1:5000/contacto', {
+  const res = await fetch('https://aurajoyasbackend.onrender.com', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ nombre, correo, mensaje })
